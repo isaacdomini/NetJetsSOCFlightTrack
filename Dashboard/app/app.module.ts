f4384a941f235627458c2740389c00d8b229ed00
shantanu94 <bhardwaj.35@osu.edu>
2017-02-03 14:01:20 -0500

    added bootstrap

--- Dashboard/app/app.module.ts
+++ Dashboard/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import {LoginComponent } from './login/login.component'
+import {NavbarComponent } from './navbar/navbar.component'
+
+import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, LoginComponent ],
+  declarations: [ AppComponent, LoginComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
