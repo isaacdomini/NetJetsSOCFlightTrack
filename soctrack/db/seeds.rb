ba533db283e619cd181ca13019f5201e45086a62
Cyriac Domini <cyriacd@outlook.com>
2017-02-16 11:16:26 -0500

    seeds

--- soctrack/db/seeds.rb
+++ soctrack/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+domains = ["osu.edu", "gmail.com", "netjets.com"]
+roles = ["AB","OS","CS","DX","OPS","MX","ITP","SC"]
+i=0
+for r in roles
+  for d in domains
+    u = User.new(:email => r+"@"+d, :password => 'password', :password_confirmation => 'password', :role => r)
+    u.save
+    puts i
+    i= i+1
+  end
+end
