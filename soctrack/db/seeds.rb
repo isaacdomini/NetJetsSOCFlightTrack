55427dba58a114b39da30bd16ce9b248a363347f
Cyriac Domini <cyriacd@outlook.com>
2017-02-16 18:37:01 -0500

    seeds for name updated

--- soctrack/db/seeds.rb
+++ soctrack/db/seeds.rb
@@ -7,10 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 domains = ["osu.edu", "gmail.com", "netjets.com"]
 roles = ["AB","OS","CS","DX","OPS","MX","ITP","SC"]
+lname = ["Buckeye", "Doe", "Bedich", "Nanayakkara", "Bhardwaj", "Domini"]
+fname = ["Josh", "Jacob", "Jonathan", "Jared", "John", "Brutus"]
 i=0
 for r in roles
   for d in domains
-    u = User.new(:email => r+"@"+d, :password => 'password', :password_confirmation => 'password', :role => r)
+    u = User.new(:email => r+"@"+d, :password => 'password', :password_confirmation => 'password', :role => r, :name => fname.sample+" "+lname.sample+" ("+r+")")
+    print u.email + " " + u.name;
     u.save
     puts i
     i= i+1
