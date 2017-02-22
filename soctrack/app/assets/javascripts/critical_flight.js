feab7695be71651b46259c4bb40948a00ae5297f
Cyriac Domini <me@cyriacdomini.com>
2017-02-22 10:29:20 -0500

    merge fix and buttons

--- soctrack/app/assets/javascripts/critical_flight.js
+++ soctrack/app/assets/javascripts/critical_flight.js
@@ -265,9 +265,11 @@
             "order": [[1, 'asc']],
             dom: 'l<"toolbar">frtip',
             initComplete: function(){
-              $("div.toolbar").html('<button type="button" class="btn btn-primary" onclick="showAll(this)" id="showBtn">Show All</button>'+
-                                    '<button type="button" class="btn btn-primary" onclick="hideAll(this)" id="hideBtn">Hide All</button>'+
-                                    +'<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addFlightModal">Add Critical Flight</button>');
+              $("div.toolbar").html('<div class="btn-group" role="toolbar" aria-label="...">'+
+                                    '<button type="button" class="btn" role="group" aria-label="..." onclick="showAll(this)" id="showBtn">Show All</button>'+
+                                    '<button type="button" class="btn" role="group" aria-label="..." onclick="hideAll(this)" id="hideBtn">Hide All</button>'+
+                                    '<button type="button" class="btn btn-primary" role="group" aria-label="..." data-toggle="modal" data-target="#addFlightModal">Add Critical Flight</button>'+
+                                    '</div>');
 
             }
         } );
