78be9baeb536b73e7c3fe916d82fc1438a18a242
Cyriac Domini <me@cyriacdomini.com>
2017-02-25 22:51:50 -0500

    heroku deploy script fix and/or improvements

--- herokudeploy.sh
+++ herokudeploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-heroku config:set BUNDLE_WITHOUT="development:test"
 git subtree push --prefix soctrack heroku master
 heroku pg:reset --confirm soctrackdev
-heroku run rails db:migrate VERSION=0
 heroku run rails db:migrate
 heroku run rake db:seed
