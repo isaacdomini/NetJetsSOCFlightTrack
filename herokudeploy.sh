3eaa045a14c59ab96944725f3c4732a351431b55
Cyriac Domini <me@cyriacdomini.com>
2017-03-05 23:03:08 -0500

    force deploy command

--- herokudeploy.sh
+++ herokudeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git subtree push --prefix soctrack heroku master
+# git push heroku `git subtree split --prefix soctrack master`:master --force
 heroku pg:reset --confirm soctrackdev
 heroku run rails db:migrate
 heroku run rake db:seed
